14c8351658bfe84223e551d908fc4d45640a0760
Cloudy Young <cloudyyoung74@outlook.com>
2021-06-11 02:17:19 +0800

    Update start animation

--- index.js
+++ index.js
@@ -28,7 +28,6 @@ setTimeout(function () {
 
 
 // KONVA
-
 // Initialization - Stage
 let stage = new Konva.Stage({
     container: $(".konva").get(0),
@@ -74,15 +73,19 @@ let kvs = {
 
     // charas
     "shizuku": { position: positions.BOTTOM_LEFT, start: { x: kv_chara_width * -0.1, y: kv_chara_height * -0.06, rotate: 12, scale: 1.2, delay: 470 } },
-    "honoka": { position: positions.TOP_RIGHT, start: { x: kv_chara_width * -0.1, y: kv_chara_height * -0.2, rotate: 20, scale: 1.44, delay: 200 } },
+    "honoka": { position: positions.TOP_RIGHT, start: { x: kv_chara_width * -0.1, y: kv_chara_height * -0.2, rotate: 20, scale: 1.32, delay: 200 } },
     "miyuki": { position: positions.BOTTOM_RIGHT, start: { x: kv_chara_width * -0.34, y: kv_chara_height * -0.5, rotate: 24, scale: 1.62, delay: 100 } },
 };
 
 
 for (let [kv, attr] of Object.entries(kvs)) {
     console.log(kv);
+
     let kv_img = new Image();
     kv_img.src = `img/${kv}.png`;
+    kv_img.onload = function () {
+        attr.loaded = true;
+    }
 
     attr.konva = new Konva.Image({
         image: kv_img,
@@ -107,15 +110,30 @@ for (let [kv, attr] of Object.entries(kvs)) {
         rotation: 0,
         easing: mahouka_bezier,
     });
+
     attr.tween_opacity = new Konva.Tween({
         node: attr.konva,
         duration: 0.4,
         opacity: 1,
         easing: Konva.Easings.EaseInOut,
     });
-    setTimeout(() => attr.tween.play(), 2000 + (attr.start.delay * 2 || 0));
-    setTimeout(() => attr.tween_opacity.play(), 2000 + (attr.start.delay * 2 || 0));
 }
+
+let all_loaded_detection = setInterval(() => {
+    let all_loaded = true;
+    for (let [kv, attr] of Object.entries(kvs)) {
+        if (!attr.loaded) {
+            all_loaded = false;
+            break;
+        }
+    }
+
+    if (all_loaded) {
+        console.warn("kvs all loaded");
+        clearInterval(all_loaded_detection);
+        start();
+    }
+}, 100);
 
 setTimeout(() => stage.batchDraw(), 500);
 
@@ -123,4 +141,19 @@ setTimeout(() => stage.batchDraw(), 500);
 function mahouka_bezier(t, b, c, d) {
     let a = bezier(0.01, 0.9, 0, 0.79);
     return b + a(t / d) / c;
+}
+
+
+function start() {
+    // kvs start animation
+    for (let [kv, attr] of Object.entries(kvs)) {
+        setTimeout(() => attr.tween.play(), 2000 + (attr.start.delay || 0));
+        setTimeout(() => attr.tween_opacity.play(), 2000 + (attr.start.delay || 0));
+    }
+
+    // Logo
+    setTimeout(() => $(".logo").addClass("animated fadeIn"), 3200);
+
+    // Glow
+    setTimeout(() => $(".glow").addClass("animated fadeInGlow"), 1200);
 }
