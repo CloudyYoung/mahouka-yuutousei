ef679fc528299f67f4a420f2c6c1b3523362f94c
Cloudy Young <cloudyyoung74@outlook.com>
2021-06-12 23:13:17 +0800

    Update index.js

--- index.js
+++ index.js
@@ -7,20 +7,6 @@ $("audio").each((index, each) => {
     each.volume = 0.02;
 });
 
-$.mouse = function (e) {
-    let wr = e.clientX / width;
-    let hr = e.clientY / height;
-
-    let x = e.clientX;
-    let y = e.clientY;
-
-    $(".kv").css({ "--x": `${x}px`, "--y": `${y}px` });
-
-    // console.log(x, y);
-};
-
-$.mouse({ "clientX": 0, "clientY": 0 });
-$(document).on('mousemove', $.mouse);
 
 
 // KONVA
@@ -61,16 +47,16 @@ let positions = {
 // kv_charas
 let kvs = {
     // cads
-    "cad-1": { origin: { width: 156, height: 391, x: 171, y: 683, position: positions.CENTER_LEFT }, start: { y: kv_chara_height * -0.35, rotate: 18, scale: 1.3, delay: 450 } },
-    "cad-2": { origin: { width: 434, height: 400, x: 0, y: 108, position: positions.CENTER_LEFT }, start: { y: kv_chara_height * -0.3, rotate: 1, scale: 1.5, delay: 400 } },
-    "cad-3": { origin: { width: 732, height: 553, x: 616, y: 0, position: positions.TOP_CENTER }, start: { x: kv_chara_width * 0.02, y: kv_chara_height * -0.3, rotate: 20, scale: 1.44, delay: 350 } },
+    "cad-1": { origin: { width: 156, height: 391, x: 171, y: 683, position: positions.CENTER_LEFT }, start: { y: -0.35, rotate: 18, scale: 1.3, delay: 450 } },
+    "cad-2": { origin: { width: 434, height: 400, x: 0, y: 108, position: positions.CENTER_LEFT }, start: { y: -0.3, rotate: 1, scale: 1.5, delay: 400 } },
+    "cad-3": { origin: { width: 732, height: 553, x: 616, y: 0, position: positions.TOP_CENTER }, start: { x: 0.02, y: -0.3, rotate: 20, scale: 1.44, delay: 350 } },
     "cad-4": { origin: { width: 288, height: 197, x: 1466, y: 318, position: positions.TOP_CENTER }, start: { rotate: 8, scale: 1.4, delay: 300 } },
     "cad-5": { origin: { width: 234, height: 239, x: 1825, y: 22, position: positions.TOP_CENTER }, start: { rotate: 5, scale: 1.4, delay: 250 } },
 
     // charas
-    "shizuku": { origin: { width: 1638, height: 1020, x: 0, y: 980, position: positions.BOTTOM_LEFT }, start: { x: kv_chara_width * -0.16, y: kv_chara_height * -0.24, rotate: 14, scale: 1.4, delay: 470 } },
-    "honoka": { origin: { width: 1193, height: 1120, x: 2124, y: 2, position: positions.TOP_RIGHT }, start: { x: kv_chara_width * -0.1, y: kv_chara_height * -0.2, rotate: 14, scale: 1.4, delay: 200 } },
-    "miyuki": { origin: { width: 1809, height: 1691, x: 1746, y: 309, position: positions.BOTTOM_RIGHT }, start: { x: kv_chara_width * -0.24, y: kv_chara_height * -0.6, rotate: 26, scale: 1.52, delay: 100 } },
+    "shizuku": { origin: { width: 1638, height: 1020, x: 0, y: 980, position: positions.BOTTOM_LEFT }, start: { x: -0.16, y: -0.24, rotate: 14, scale: 1.4, delay: 470 } },
+    "honoka": { origin: { width: 1193, height: 1120, x: 2124, y: 2, position: positions.TOP_RIGHT }, start: { x: 0.1, y: 0.2, rotate: 14, scale: 1.4, delay: 200 } },
+    "miyuki": { origin: { width: 1809, height: 1691, x: 1746, y: 309, position: positions.BOTTOM_RIGHT }, start: { x: -0.02, y: 0.08, rotate: 30, scale: 1.46, delay: 100 } },
 };
 
 
@@ -88,7 +74,8 @@ for (let [kv, attr] of Object.entries(kvs)) {
 
     let calculated_origin_x = kv_chara_width * (attr.origin.x / kv_real_width);
     let calculated_origin_y = kv_chara_height * (attr.origin.y / kv_real_height);
-    attr.position = attr.origin.position(calculated_origin_x, calculated_origin_y)
+    attr.position = attr.origin.position(calculated_origin_x, calculated_origin_y);
+
     console.log(kv, attr.position.x, attr.position.y, attr.position.x + (attr.start.x || 0), attr.position.y + (attr.start.y || 0));
 
     attr.konva = new Konva.Image({
@@ -96,8 +83,8 @@ for (let [kv, attr] of Object.entries(kvs)) {
         width: attr.size.width || kv_chara_width,
         height: attr.size.height || kv_chara_height,
         opacity: attr.opacity || 0,
-        x: attr.position.x + (attr.start.x || 0),
-        y: attr.position.y + (attr.start.y || 0),
+        x: attr.position.x * (1 + attr.start.x || 1),
+        y: attr.position.y * (1 + attr.start.y || 1),
         scaleX: attr.start.scale || 1,
         scaleY: attr.start.scale || 1,
         rotation: attr.start.rotate || 0,
@@ -161,3 +148,23 @@ function start() {
     // Glow
     setTimeout(() => $(".glow").addClass("animated fadeInGlow"), 2200);
 }
+
+
+
+$.mouse = function (e) {
+    let wr = e.clientX / width;
+    let hr = e.clientY / height;
+
+    let x = e.clientX;
+    let y = e.clientY;
+
+    // $(".kv").css({ "--x": `${x}px`, "--y": `${y}px` });
+    // kvs["miyuki"].konva.offset({ x: x, y: y });
+    // stage.batchDraw();
+
+
+    // console.log(x, y);
+};
+
+$.mouse({ "clientX": 0, "clientY": 0 });
+$(document).on('mousemove', $.mouse);
