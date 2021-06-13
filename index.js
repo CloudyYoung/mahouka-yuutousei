fd18291d92a3d6c6eaecd3e14dd97e1b49f2ae65
Cloudy Young <cloudyyoung74@outlook.com>
2021-06-14 02:21:24 +0800

    Update smooth movement

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 let width = window.screen.width;
 let height = window.screen.height;
-let global_move_rate = 0.02;
+let global_move_rate = 0.026;
 
 
 $("audio").each((index, each) => {
@@ -26,7 +26,7 @@ stage.add(kvs_layer);
 let kv_real_width = 3555;
 let kv_real_height = 2000;
 
-let kv_chara_width = width * 1.02;
+let kv_chara_width = width * (1.01 + global_move_rate);
 let kv_chara_height = kv_real_height * (kv_chara_width / kv_real_width);
 
 let kv_chara_x = (width - kv_chara_width);
@@ -50,44 +50,44 @@ let kvs = {
     "miyuki": {
         origin: { width: 1877, height: 1792, x: 1746, y: 309, position: positions.BOTTOM_RIGHT, zIndex: 7 },
         start: { x: kv_chara_width * -0.12, y: kv_chara_height * 0.2, rotate: 24, scale: 1.42, delay: 100 },
-        move: { x: 1.7, y: 1.4 },
+        move: { x: 1.4, y: 1.8 },
     },
     "honoka": {
         origin: { width: 1193, height: 1120, x: 2124, y: 2, position: positions.TOP_RIGHT, zIndex: 6 },
         start: { x: kv_chara_width * 0.04, y: kv_chara_height * 0.14, rotate: 14, scale: 1.24, delay: 200 },
-        move: { x: 1.2, y: 1.8 },
+        move: { x: 1.8, y: 2.4 },
     },
     "shizuku": {
         origin: { width: 1685, height: 1443, x: -47, y: 980, position: positions.BOTTOM_LEFT, zIndex: 5 },
         start: { x: kv_chara_width * -0.06, y: kv_chara_height * -0.02, rotate: 14, scale: 1.2, delay: 500 },
-        move: { x: 1.4, y: 1.2 },
+        move: { x: 1.2, y: 1.2 },
     },
 
     // cads
     "cad-1": {
         origin: { width: 156, height: 391, x: 171, y: 683, position: positions.CENTER_LEFT, zIndex: 4 },
         start: { x: kv_chara_width * 0.1, y: kv_chara_height * -0.1, rotate: 8, scale: 1.3, delay: 450 },
-        move: { x: 0.8, y: 0.8 },
+        move: { x: 0.8, y: 1.8 },
     },
     "cad-2": {
         origin: { width: 434, height: 400, x: 0, y: 108, position: positions.CENTER_LEFT, zIndex: 3 },
         start: { x: kv_chara_width * 0.06, y: kv_chara_height * 0.02, rotate: 12, scale: 1.3, delay: 400 },
-        move: { x: 0.7, y: 0.8 },
+        move: { x: 1, y: 1.3 },
     },
     "cad-3": {
         origin: { width: 732, height: 553, x: 616, y: 0, position: positions.TOP_CENTER, zIndex: 2 },
         start: { x: kv_chara_width * 0.1, y: kv_chara_height * 0.08, rotate: 20, scale: 1.44, delay: 350 },
-        move: { x: 0.9, y: 0.8 },
+        move: { x: 1.4, y: 1 },
     },
     "cad-4": {
         origin: { width: 288, height: 197, x: 1466, y: 318, position: positions.TOP_CENTER, zIndex: 1 },
         start: { x: kv_chara_width * 0.16, y: kv_chara_height * 0.1, rotate: 8, scale: 1.4, delay: 300 },
-        move: { x: 1.2, y: 1.2 },
+        move: { x: 1.85, y: 1.4 },
     },
     "cad-5": {
         origin: { width: 234, height: 239, x: 1825, y: 22, position: positions.TOP_CENTER, zIndex: 0 },
         start: { x: kv_chara_width * 0.08, y: kv_chara_height * 0.2, rotate: 5, scale: 1.6, delay: 250 },
-        move: { x: 1, y: 1.1 },
+        move: { x: 1.65, y: 1.2 },
     },
 };
 
@@ -150,7 +150,11 @@ for (let [kv, attr] of Object.entries(kvs)) {
         rotation: attr.start.rotate || 0,
         offsetX: attr.offset.x,
         offsetY: attr.offset.y,
+        opacity: attr.opacity || 0,
     });
+
+    // Movement group for kv offset & tween "smooth movement"
+    attr.konva_move = new Konva.Group();
 
     // Actual kv, size as it is
     // Apply mouse movement offset
@@ -160,11 +164,12 @@ for (let [kv, attr] of Object.entries(kvs)) {
         height: attr.kv_size.height,
         x: attr.kv_position.x,
         y: attr.kv_position.y,
-        opacity: attr.opacity || 0,
     });
 
+    // Add to layers: Group -> Movement Group -> Image kv
     kvs_layer.add(attr.konva_group);
-    attr.konva_group.add(attr.konva_kv);
+    attr.konva_group.add(attr.konva_move);
+    attr.konva_move.add(attr.konva_kv);
 
     attr.tween_start = new Konva.Tween({
         node: attr.konva_group,
@@ -178,7 +183,7 @@ for (let [kv, attr] of Object.entries(kvs)) {
     });
 
     attr.tween_opacity = new Konva.Tween({
-        node: attr.konva_kv,
+        node: attr.konva_group,
         duration: 0.4,
         opacity: 1,
         easing: Konva.Easings.EaseInOut,
@@ -237,8 +242,20 @@ $.mouse = function (e) {
     let y = e.clientY * global_move_rate;
 
     for (let [kv, attr] of Object.entries(kvs)) {
-        attr.konva_kv.offsetX(x * attr.move.x);
-        attr.konva_kv.offsetY(y * attr.move.y);
+        let total_x = x * attr.move.x;
+        let total_y = y * attr.move.y;
+
+        attr.konva_kv.offsetX(total_x * 0.55);
+        attr.konva_kv.offsetY(total_y * 0.55);
+
+        attr.tween_move = new Konva.Tween({
+            node: attr.konva_move,
+            duration: 10,
+            offsetX: total_x * 0.45,
+            offsetY: total_y * 0.45,
+            easing: mahouka_bezier,
+        });
+        attr.tween_move.play();
     }
     stage.batchDraw();
 };
